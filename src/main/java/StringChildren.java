36acd7416883fed35aafe8e242c2f4beafe0ce79
arunpatil <arunpatilmailbox@gmail.com>
2021-09-29 16:27:49 -0600

    Get all permutation for a given string

---
+++ src/main/java/StringChildren.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringChildren {
+
+    public static void main(String[] args) {
+
+        getChildren("hit").forEach(System.out::println);
+    }
+
+    private static List<String> getChildren(String parent) {
+
+        List<String> children = new ArrayList<>();
+
+        char[] parentCharArray = parent.toCharArray();
+
+        for (int i=0; i<parentCharArray.length; i++) {
+            char temp = parentCharArray[i];
+            for (char c = 'a' ; c <= 'z' ; c++) {
+                if (temp != c) {
+                    parentCharArray[i] = c;
+                    children.add(new String(parentCharArray));
+                }
+            }
+            parentCharArray[i] = temp;
+        }
+        return children;
+    }
+
+}

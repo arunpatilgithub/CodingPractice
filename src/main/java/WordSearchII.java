64bc296c8250221920a4276eba487b0aadafcd07
arunpatil <arunpatilmailbox@gmail.com>
2021-10-20 20:57:21 -0600

    212. Word Search II
    https://leetcode.com/problems/word-search-ii/

--- src/main/java/WordSearchII.java
+++ src/main/java/WordSearchII.java
@@ -1,60 +1,119 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class WordSearchII {
 
-    static TrieNode root;
-
     static class TrieNode {
-        //boolean isWord;
+        boolean isWord;
         TrieNode[] children;
-
         public TrieNode() {
             children = new TrieNode[128];
         }
     }
 
-    public WordSearchII() {
-        root = new TrieNode();
+    public static void insert(String word, TrieNode root) {
+
+        TrieNode crawler = root;
+
+        for (int i=0; i<word.length(); i++) {
+            if (crawler.children[word.charAt(i) - 'a'] == null) {
+                crawler.children[word.charAt(i) - 'a'] = new TrieNode();
+            }
+            crawler = crawler.children[word.charAt(i) - 'a'];
+        }
+        crawler.isWord = true;
+
     }
 
-    public static TrieNode insert(TrieNode node, char c) {
+    public static boolean isWordPresent(String word, TrieNode root) {
 
-        TrieNode crawler = node;
-        if (crawler.children[c] == null) {
-            crawler.children[c] = new TrieNode();
+        TrieNode crawler = root;
+        for (int i=0; i<word.length(); i++) {
+            if (crawler.children[word.charAt(i) - 'a'] == null) {
+                return false;
+            }
+            crawler = crawler.children[word.charAt(i) - 'a'];
         }
+        return crawler.isWord;
 
-        return crawler.children[c];
+    }
+
+    public static boolean wordStartsWith(String word, TrieNode root) {
+
+        TrieNode crawler = root;
+
+        for (int i=0; i<word.length(); i++) {
+            if (crawler.children[word.charAt(i) - 'a'] == null) {
+                return false;
+            }
+            crawler = crawler.children[word.charAt(i) - 'a'];
+        }
+        return true;
+
     }
 
     public static void main(String[] args) {
 
-        char[][] board = {{'o','a','a','n'},{'e','t','a','e'},{'i','h','k','r'},{'i','f','l','v'}};
-        String[] words = {"oath","pea","eat","rain"};
+//        char[][] board = {{'o','a','a','n'},{'e','t','a','e'},{'i','h','k','r'},{'i','f','l','v'}};
+//        String[] words = {"oath","pea","eat","rain"};
 
-        findWords(board, words).stream().forEach(System.out::println);
+//        char[][] board = {{'o','a','b','n'},{'o','t','a','e'},{'a','h','k','r'},{'a','f','l','v'}};
+
+//        String[] words = {"oa", "oaa"};
+
+        char[][] board = {{'o','a','a','n'},{'e','t','a','e'},{'i','h','k','r'},{'i','f','l','v'}};
+        String[] words = {"oath","pea","eat","rain","oathi","oathk","oathf","oate","oathii","oathfi","oathfii"};
+        
+        List<String> searchedWords = findWords(board, words);
+
+        System.out.println("*****Words in grid*****");
+        searchedWords.forEach(System.out::println);
     }
 
     public static List<String> findWords(char[][] board, String[] words) {
 
-        root = new TrieNode();
-        boolean[][] markerGrid = new boolean[board.length][board[0].length];
+         TrieNode root = new TrieNode();
+        List<String> searchedWords = new ArrayList<>();
+
+        for(String word : words) {
+            insert(word, root);
+        }
+
         for (int i=0; i<board.length; i++) {
             for (int j=0; j<board[i].length; j++) {
-                generateTrie(root, i, j, board, markerGrid);
+                boolean[][] markerGrid = new boolean[board.length][board[0].length];
+                dfs(board, markerGrid, "",i, j, searchedWords, root);
             }
         }
-
-        return null;
+        return searchedWords;
     }
 
-    private static void generateTrie(TrieNode trieNode, int i, int j, char[][] board, boolean[][] markerGrid) {
+    private static void dfs(char[][] board, boolean[][] markerGrid, String subString, int row, int column, List<String> searchedWords,
+                            TrieNode root) {
 
-        if (i >=0 && j >= 0 && i < board.length && j < board[i].length && !markerGrid[i][j]) {
-            TrieNode trie = insert(trieNode,board[i][j]);
-            generateTrie(trie, i , j + 1, board, markerGrid);
-            generateTrie(trieNode, i + 1, j, board, markerGrid);
+        if (row < 0 || row > board.length - 1 || column < 0 || column > board[row].length - 1 || markerGrid[row][column]) {
+            return;
         }
 
+        subString = subString + board[row][column];
+
+        if ("" != subString && !wordStartsWith(subString, root)) {
+            return;
+        }
+
+        if (isWordPresent(subString, root) && !searchedWords.contains(subString)) {
+            searchedWords.add(subString);
+        }
+
+        markerGrid[row][column] = true;
+
+        dfs(board, markerGrid, subString, row + 1, column, searchedWords, root);
+        dfs(board, markerGrid, subString, row, column + 1, searchedWords, root);
+        dfs(board, markerGrid, subString, row - 1, column, searchedWords, root);
+        dfs(board, markerGrid, subString, row, column - 1, searchedWords, root);
+
+        markerGrid[row][column] = false;
     }
+
 }
